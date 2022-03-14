4307c97c22113419a78059615286a92016a004fd
Viktor Levente Tóth <toth.viktor.levente@gmail.com>
2022-03-14 20:13:24 +0100

    feat: app element init

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import logo from './logo.svg'
+import ContentView from './ContentView'
 import './App.css'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import Thread from './Thread'
 
 function App() {
+  const [contentHash, setContentHash] = useState(window.location.hash.slice(1))
+
+  useEffect(() => {
+    const valami = () => {
+      console.log('hashchange1', window.location.hash)
+      setContentHash(window.location.hash.slice(1))
+    }
+    window.addEventListener('hashchange', valami)
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <h2>633chan</h2>
+        <ContentView contentHash={contentHash} />
       </header>
+      <div className="633chan-body">
+        <Thread />
+      </div>
     </div>
   )
 }
